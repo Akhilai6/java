96284fcd09b16b6849412ee0c16ad21067651e81
Akhilai6 <154301436+Akhilai6@users.noreply.github.com>
2024-08-22 12:42:02 +0530

    Add files via upload

--- calculator.java
+++ calculator.java
@@ -1,15 +1,39 @@
-package javalab;
+package practiceprograms;
+import java.util.Scanner;
 
 public class calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int sum=0;
-		int n = 3;
-		for (int i = 0;i<=n;i++)
-		{ sum = sum+i;
+		System.out.print("Enter an operator: ");
+		Scanner n = new Scanner(System.in);
+		String symbol = n.nextLine();
+		System.out.println("Enter a number: ");
+		Scanner m = new Scanner(System.in);
+		int a = m.nextInt();
+		System.out.println("Enter a number: ");
+		Scanner o = new Scanner(System.in);
+		int b = o.nextInt();
+		switch(symbol){
+			case "+":
+				System.out.print(a+b);
+				break;
+			case "-":
+				System.out.print(a-b);
+			case "*":
+				System.out.print(a*b);
+				
+			case "/":
+				System.out.print(a/b);
+			case "%":
+				System.out.print(a%b);
+			default:
+				System.out.print("enter valid symbol");
+				
+				
+		}
+		
 
-	    }
-   System.out.println(sum);
-}
+	}
+
 }
